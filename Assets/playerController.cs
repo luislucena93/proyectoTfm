e3c4b833b8e29c821c10a74c72b19119cc28420f
luislucena93 <luislucena93@gmail.com>
2022-04-11 21:14:37 -0400

    third person perspective

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -31,15 +31,37 @@ public class playerController : MonoBehaviour
 
     private void move()
     {
-        transform.Rotate(0, moverAction.ReadValue<Vector2>().x * velocidadRotation * Time.deltaTime, 0);
+        float rotationDirection = moverAction.ReadValue<Vector2>().x ;
+        transform.Rotate(0, rotationDirection * velocidadRotation * Time.deltaTime, 0);
         float curSpeed = velocidadChar * moverAction.ReadValue<Vector2>().y;
+        curSpeed = curSpeed < 0 ? curSpeed / 4 : curSpeed;
         transform.Translate(Vector3.forward * (curSpeed) * Time.deltaTime);
-        if (curSpeed != 0)
+        Debug.Log(curSpeed);
+        if (curSpeed > 0)
         {
-            animator.SetBool("isMoving", true);   
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isBacking", false);
+        } else  if (curSpeed < 0)
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isBacking", true);
         } else
         {
-            animator.SetBool("isMoving", false);
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isBacking", false);
+        }
+        if (rotationDirection > 0)
+        {
+            animator.SetBool("turningRight", true);
+            animator.SetBool("turningLeft", false);
+        } else if (rotationDirection < 0)
+        {
+            animator.SetBool("turningRight", false);
+            animator.SetBool("turningLeft", true);
+        } else
+        {
+            animator.SetBool("turningRight", false);
+            animator.SetBool("turningLeft", false);
         }
     }
 
